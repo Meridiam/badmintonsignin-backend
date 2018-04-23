0a08f0607ebbd41a97fd87d6f4cef99f9dff138b
Jerry Peng <png.jrry@gmail.com>
2018-04-22 22:50:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const request = require('request');
+const redis = require('redis');
+
+var db = process.env.REDIS_URL || "//127.0.0.1:6379";
+const client = redis.createClient(db);
+
+const url = "https://umsporttool.umd.edu/api/Rosters/get?clubID=Bdmtn";
+
+var app = express();
+
+app.get('/updateroster', function (req, res) {
+    var names = [];
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const members = JSON.parse(body)["data"];
+            for (var i = 0; i < members.length; i++) {
+                var firstname = members[i]["firstName"].charAt(0).toUpperCase() + members[i]["firstName"].substring(1).toLowerCase();
+                var lastname = members[i]["lastName"].charAt(0).toUpperCase() + members[i]["lastName"].substring(1).toLowerCase();
+                names.push(firstname+" "+lastname);
+            }
+            client.set("roster", names.join(","), redis.print);
+        }
+    });
+});
+
+app.get('/getroster', function (req, res) {
+    client.get("roster", function(err, reply) {
+        res.send(reply);
+    });
+});
+
+var port = process.env.PORT || 3000; //select your port or let it pull from your .env file
+app.listen(port);
+console.log("listening on " + port + "!");
