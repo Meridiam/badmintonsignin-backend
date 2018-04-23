dbcb74ad8ff18049353f70abceec5f32132f776b
Jerry Peng <png.jrry@gmail.com>
2018-04-23 01:17:14 -0400

    adding CORS

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const client = redis.createClient(db);
 const url = "https://umsporttool.umd.edu/api/Rosters/get?clubID=Bdmtn";
 
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 app.use(allowCrossDomain);
 
 app.get('/updateroster', function (req, res) {
